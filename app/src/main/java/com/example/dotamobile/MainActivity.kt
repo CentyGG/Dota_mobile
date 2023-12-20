b47aafeb2eaa1d68f0a11d2d8e6786d560962c17
Sonnnyyy04 <sanseinikolai83@gmail.com>
2023-12-20 00:25:02 +0000

    update

--- app/src/main/java/com/example/dotamobile/MainActivity.kt
+++ app/src/main/java/com/example/dotamobile/MainActivity.kt
@@ -2,7 +2,15 @@ package com.example.dotamobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dotamobile.data.OpenDotaApi
+import com.example.dotamobile.dataClasses.Hero
 import com.example.dotamobile.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding:ActivityMainBinding
@@ -10,5 +18,16 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        val retrofit = Retrofit.Builder().baseUrl("https://api.opendota.com/api/")
+            .addConverterFactory(GsonConverterFactory.create()).build()
+        val api = retrofit.create(OpenDotaApi::class.java)
+        binding.button.setOnClickListener{
+            CoroutineScope(Dispatchers.IO).launch {
+                val heroes = api.getHeroes()
+                runOnUiThread{
+                    binding.tv.text = heroes.name
+                }
+            }
+        }
     }
 }
